422bf77b2cebce4170bef89803bdf6f02892ac7b
kodukha <khrystynanatalyuk@gmail.com>
2017-02-22 11:29:13 +0200

    added initial js and json

---
+++ app.js
@@ -0,0 +1,13 @@
+
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World 3000!');
+});
+app.use('/', express.static('views'));
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
+
